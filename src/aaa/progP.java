0bc2f4f19eb09e23c98af17429afd88b30967105
mahdi <mahdi.lengliz@semsame.com.tn>
2020-03-05 11:24:45 +0000

    first commit

---
+++ src/aaa/progP.java
@@ -0,0 +1,11 @@
+package aaa;
+
+public class progP {
+
+	public static void main(String[] args) {
+		testt t=new testt(5,4,3);
+		System.out.println("la somme est "+t.somme());
+		System.out.print("");
+	}
+
+}
---
+++ src/aaa/testt.java
@@ -0,0 +1,18 @@
+package aaa;
+
+public class testt {
+	private  int a;
+	private double b;
+	private double c;
+	private double d;
+	public  testt(int a,double b,double c) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	}
+	public double somme() {
+		return d=a+b+c;
+	}
+	public int x;
+
+}
